136e341e0896f7cf792d6f75dedf1e1c6f6ac592
Yedidya <yedidya.abergel@gmail.com>
2023-01-10 21:29:40 +0200

    Import missing models to file

--- src/modules/boxview/controlbar/handleNextButton.js
+++ src/modules/boxview/controlbar/handleNextButton.js
@@ -1,6 +1,10 @@
 import { boxviewThumbnailsTrack } from '../../document/docConstants.js';
 import getBoxviewActiveMediaUrl from '../mainContent/getBoxviewActiveMediaUrl.js';
+import createImageElement from '../mainContent/createImageElement.js';
+import createIframeElement from '../mainContent/createIframeElement';
+import createVideoElement from '../mainContent/createVideoElement.js';
 import toggleActiveThumbnail from '../thumbnailsTrack/toggleActiveThumbnail.js';
+import createIframeElement from '../mainContent/createIframeElement.js';
 
 export default function handleNextButton(preparedMediaElements) {
   const thumbnailsTrackList = [...boxviewThumbnailsTrack.children];
