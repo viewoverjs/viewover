ff046d21917d3b64eac6f9fa09398d730f68033d
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-01-25 03:23:59 +0200

    npm run build

--- src/js/viewover.js
+++ src/js/viewover.js
@@ -3,7 +3,9 @@
 import setSettings from './utils/settings.js';
 import { applyViewoverOnDoc } from './document/applyViewoverOnDoc.js';
 
-export default function viewover(options) {
+function viewover(options) {
   const settings = setSettings(options);
   applyViewoverOnDoc(settings);
 }
+
+module.exports = viewover;
