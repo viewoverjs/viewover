7523a006256a239d11d293a287a4285d2eeb4e6c
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-02-02 16:21:33 +0200

    Update Config files

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,7 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Shared options for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
