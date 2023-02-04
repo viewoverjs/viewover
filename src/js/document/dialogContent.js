7afea79355a003dce1696a046d01914c20bfdb85
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-02-04 22:05:53 +0200

    Adding CSS in JS string

---
+++ src/js/document/dialogContent.js
@@ -0,0 +1,293 @@
+export const dialogContent = `
+<div id="viewover-container" class="viewover__container">
+  <div id="control-bar" class="viewover__control-bar">
+    <div
+      class="viewover__controls-buttons-wrapper viewover__controls-buttons-wrapper_type_share"
+    >
+      <div class="share-options-buttons">
+        <div>
+          <svg
+            class="svg-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            width="30px"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25c-.25-.16-.4-.43-.4-.72 0-.67.73-1.07 1.3-.72L12 11l6.7-4.19c.57-.35 1.3.05 1.3.72 0 .29-.15.56-.4.72z"
+            />
+          </svg>
+        </div>
+        <div>
+          <svg
+            class="svg-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            width="30px"
+            fill="none"
+            viewBox="0 0 512 512"
+          >
+            <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <path
+              d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"
+            />
+          </svg>
+        </div>
+        <div>
+          <svg
+            class="svg-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            width="30px"
+            fill="none"
+            viewBox="0 0 448 512"
+          >
+            <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <path
+              d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
+            />
+          </svg>
+        </div>
+      </div>
+      <button class="button share-button" id="share-button">
+        <svg
+          class="svg-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="28px"
+          viewBox="0 0 24 24"
+          width="28px"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"
+          />
+        </svg>
+      </button>
+      <button class="button download-button" id="download-button">
+        <svg
+          class="svg-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          enable-background="new 0 0 24 24"
+          height="32px"
+          viewBox="0 0 24 24"
+          width="32px"
+        >
+          <g><rect fill="none" height="24" width="24" /></g>
+          <g><path d="M5,20h14v-2H5V20z M19,9h-4V3H9v6H5l7,7L19,9z" /></g>
+        </svg>
+      </button>
+    </div>
+    <div
+      class="viewover__controls-buttons-wrapper viewover__controls-buttons-wrapper_type_interactive"
+    >
+      <div class="viewover__auto-scroll-buttons">
+        <button
+          class="button viewover__auto-scroll-button"
+          id="auto-scroll-pause-button"
+        >
+          <svg
+            class="svg-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="32px"
+            viewBox="0 0 24 24"
+            width="32px"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+          </svg>
+        </button>
+        <button
+          class="button viewover__auto-scroll-button button_visible"
+          id="auto-scroll-play-button"
+        >
+          <svg
+            class="svg-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="32px"
+            viewBox="0 0 24 24"
+            width="32px"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M8 5v14l11-7z" />
+          </svg>
+        </button>
+      </div>
+
+      <button
+        class="button viewover__zoom-in-button viewover__zoom-button"
+        id="zoom-in-button"
+      >
+        <svg
+          class="svg-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="32px"
+          viewBox="0 0 24 24"
+          width="32px"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+          />
+          <path d="M12 10h-2v2H9v-2H7V9h2V7h1v2h2v1z" />
+        </svg>
+      </button>
+      <button
+        class="button viewover__zoom-out-button viewover__zoom-button"
+        id="zoom-out-button"
+      >
+        <svg
+          class="svg-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="32px"
+          viewBox="0 0 24 24"
+          width="32px"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z"
+          />
+        </svg>
+      </button>
+      <div id="thumbnails-buttons" class="viewover__thumbnails-buttons">
+        <button
+          class="button viewover__thumbnails-button"
+          id="thumbnails-filled-button"
+        >
+          <svg
+            class="svg-icon viewover__thumbnails-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 20 20"
+            height="32px"
+            viewBox="0 0 20 20"
+            width="32px"
+          >
+            <g>
+              <rect fill="none" height="20" width="20" />
+              <path
+                d="M3,4v12h14V4H3z M16,7.67h-2.5V5H16V7.67z M13.5,8.67H16v2.67h-2.5V8.67z M4,5h8.5v10H4V5z M13.5,15v-2.67H16V15H13.5z"
+              />
+            </g>
+          </svg>
+        </button>
+        <button
+          class="button viewover__thumbnails-button"
+          id="thumbnails-outlined-button"
+        >
+          <svg
+            class="svg-icon viewover__thumbnails-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 20 20"
+            height="32px"
+            viewBox="0 0 20 20"
+            width="32px"
+          >
+            <g>
+              <rect fill="none" height="20" width="20" />
+              <path
+                d="M17,7.33h-3V4h3V7.33z M17,12.67h-3V16h3V12.67z M17,8.33h-3v3.33h3V8.33z M13,4H3v12h10V4z"
+              />
+            </g>
+          </svg>
+        </button>
+      </div>
+      <div id="fullscreen-buttons" class="viewover__fullscreen-buttons">
+        <button
+          class="button button_visible viewover__fullscreen-button"
+          id="fullscreen-entry-button"
+        >
+          <svg
+            class="svg-icon fullscreen-entry-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="32px"
+            viewBox="0 0 24 24"
+            width="32px"
+            fill="none"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"
+            />
+          </svg>
+        </button>
+        <button
+          class="button button_hidden viewover__fullscreen-button"
+          id="fullscreen-exit-button"
+        >
+          <svg
+            class="svg-icon fullscreen-exit-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="32px"
+            viewBox="0 0 24 24"
+            width="32px"
+            fill="none"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+    <div
+      class="viewover__controls-buttons-wrapper viewover__controls-buttons-wrapper_type_close"
+    >
+      <button class="button viewover__close-button" id="close-button">
+        <svg
+          class="svg-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="32px"
+          viewBox="0 0 24 24"
+          width="32px"
+          fill="#000000"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+
+  <div id="viewover-content" class="viewover__content">
+    <div id="viewover-main-content" class="viewover__main-content">
+      <button
+        class="button viewover__navigate-button viewover__navigate-button_before"
+        id="nav-previous-button"
+      >
+        <svg
+          class="svg-icon svg-icon_nav"
+          xmlns="http://www.w3.org/2000/svg"
+          height="48px"
+          viewBox="0 0 24 24"
+          width="48px"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M15.61 7.41L14.2 6l-6 6 6 6 1.41-1.41L11.03 12l4.58-4.59z" />
+        </svg>
+      </button>
+      <button
+        class="button viewover__navigate-button viewover__navigate-button_next"
+        id="nav-next-button"
+      >
+        <svg
+          class="svg-icon svg-icon_nav"
+          xmlns="http://www.w3.org/2000/svg"
+          height="48px"
+          viewBox="0 0 24 24"
+          width="48px"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z" />
+        </svg>
+      </button>
+      <div id="viewover-media" class="viewover__media"></div>
+    </div>
+    <div id="viewover-thumbnails-track" class="viewover__thumbnails-track"></div>
+  </div>
+</div>
+
+`;
