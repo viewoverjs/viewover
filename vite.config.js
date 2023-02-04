c5f8ba3070a05bca1163f210dac4b349dbb4559d
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-02-04 22:06:43 +0200

    Change config to library mode

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `viewover.min.js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
-      }
-    }
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/js/Viewover.js'),
+      name: 'viewover',
+      // the proper extensions will be added
+      fileName: 'viewover-bundle.min'
+    },
   },
-  plugins: [
-    cssInjectedByJsPlugin(),
-  ]
 })
