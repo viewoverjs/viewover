26b8630dfafc8a0905da798b4e544c5175287d39
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-02-20 17:28:11 +0200

    add vite-plugin-babel config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
+  plugins: [
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: [
+          [require.resolve('babel-plugin-module-resolver'), {
+            root: ["./dist/"],
+            alias: {
+              viewover: "./viewover-bundle.min.js"
+            }
+          }]
+        ]
+      }
+    }),
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
